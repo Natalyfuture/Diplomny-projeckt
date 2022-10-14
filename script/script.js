9c0ee5e376fbb765015fcab9d4ef4e75b1415bff
Natalyfuture <111283931+Natalyfuture@users.noreply.github.com>
2022-10-14 13:09:32 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,49 @@
-$('.shop').parallax({imageSrc: '../img/shop/Combined-Shape (5).png', speed: 0.01});
+$('#shop_link').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#shop').offset().top}, 1000);
+})
+
+$('#about_link').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#about').offset().top}, 1000);
+})
+
+$('#team_link').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#team').offset().top}, 1000);
+})
+
+$('#buyer_link').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#buyer').offset().top}, 1000);
+})
+
+$('#category_link').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#category').offset().top}, 1000);
+})
+
+$('#shop_hamb').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#shop').offset().top}, 1000);
+})
+
+$('#about_hamb').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#about').offset().top}, 1000);
+})
+
+$('#team_hamb').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#team').offset().top}, 1000);
+})
+
+$('#buyer_hamb').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#buyer').offset().top}, 1000);
+})
+
+$('#category_hamb').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#category').offset().top}, 1000);
+})
